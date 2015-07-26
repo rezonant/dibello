f2eb50144cc25187065f704c35f5404ad3009c05
Liam <liam@aether>
2015-07-25 21:43:56 -0400

    Do not assume window exists

--- src/idbStandardize.js
+++ src/idbStandardize.js
@@ -1,5 +1,10 @@
 
 // Standardize any prefixed implementations of IndexedDB
-window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
+
+
+if (typeof window !== 'undefined') {
+	window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+	window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+	window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+}
