b183f66bf044908863dee8026ffd5adb9a5c599d
William Lahti <wilahti@gmail.com>
2017-09-21 12:27:27 -0700

    remove typings committed accidentally

--- src/strip-copy.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function stripCopy(obj: any): any;
--- src/utils/idb/request-to-iterable.d.ts
+++
@@ -1,5 +0,0 @@
-import "core-js/fn/symbol/async-iterator";
-/**
- * Converts an IndexedDB IDBCursor into an async iterable.
- */
-export declare function idbRequestToIterable<T>(request: IDBRequest | Promise<IDBRequest>, map?: (any) => T): AsyncIterable<T>;
