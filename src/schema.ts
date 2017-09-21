e0d0fa531ae452fa01f179807adeacb2cd103134
William Lahti <wilahti@gmail.com>
2017-09-21 15:53:27 -0700

    migrations optional in schema spec

--- src/schema.ts
+++ src/schema.ts
@@ -2,6 +2,5 @@ import { SchemaBuilder } from './schema-builder';
 
 export interface Schema {
     version: number;
-    migrations: { [ version : string ] : (schema : SchemaBuilder) => void }
+    migrations?: { [ version : string ] : (schema : SchemaBuilder) => void }
 }
-  
