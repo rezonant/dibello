1f9f4ab45c0c6e9cf58380c241e012cc4df479ac
William Lahti <wilahti@gmail.com>
2017-09-21 12:28:25 -0700

    remove additional build artifacts

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-import "./src/polyfill";
-export * from './src/constraint';
-export * from './src/database';
-export * from './src/index';
-export * from './src/repository';
-export * from './src/schema-builder';
-export * from './src/store-builder';
-export * from './src/strip-copy';
-export * from './src/transact';
