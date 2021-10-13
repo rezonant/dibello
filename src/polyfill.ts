a6bddd2e366ef2ce99f198eabb6c0128b54b7fd8
William Lahti <wilahti@gmail.com>
2021-10-12 22:31:49 -0700

    do not try to replace a perfectly good indexeddb

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -3,7 +3,10 @@
 
 if (typeof window !== 'undefined') {
 	let win : any = window;
-	win.indexedDB = win.indexedDB || win.mozIndexedDB || win.webkitIndexedDB || win.msIndexedDB;
-	win.IDBTransaction = win.IDBTransaction || win.webkitIDBTransaction || win.msIDBTransaction;
-	win.IDBKeyRange = win.IDBKeyRange || win.webkitIDBKeyRange || win.msIDBKeyRange;
+
+	if (!('indexedDB' in win)) {
+		win.indexedDB = win.indexedDB || win.mozIndexedDB || win.webkitIndexedDB || win.msIndexedDB;
+		win.IDBTransaction = win.IDBTransaction || win.webkitIDBTransaction || win.msIDBTransaction;
+		win.IDBKeyRange = win.IDBKeyRange || win.webkitIDBKeyRange || win.msIDBKeyRange;
+	}
 }
